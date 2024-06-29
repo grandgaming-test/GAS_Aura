d6bcd06531b6bd35548995240b1954ec6df58ba0
rituparno <rituparno@grandgaming.com>
2024-06-29 17:41:12 +0530

    Animation Blueprint Setup!!!

---
+++ Aura/Source/Aura/Private/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright © 2024 Global Grand Gaming Private Limited
+
+#include "AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
